6cc5bdb3c354def9712c9e988dca6305c28b73c8
David <davidwang888@gmail.com>
2020-02-06 20:08:33 -0500

    updated hw3-q1 tests to have tests with 0 as the first digit

--- hw3-q1.js
+++ hw3-q1.js
@@ -8,12 +8,19 @@ if (process.argv.length < 3) {
 const dfa = Dfa.createFromFile(process.argv[2])
 
 for (let i = 0; i < 100000; i++) {
-    const input = i.toString();
-    const result = dfa.run(input, false);
+    let input = i.toString();
+    let result = dfa.run(input, false);
     if (i <= 9000 && result !== "REJECT") {
         console.error(`Failed to reject input "${input}"`);
     } else if (i > 9000 && result !== "ACCEPT") {
         console.error(`Failed to accept input "${input}"`);
+    }
+    for (let j = 0; j < input.length; j++) {
+        input = input.substr(0, j) + "0" + input.substr(j + 1);
+        result = dfa.run(input, false);
+        if (result !== "REJECT") {
+            console.error(`Failed to reject input "${input}"`);
+        }
     }
 }
 
